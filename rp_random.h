d76bef3b4061e39cb4479bbd37ce27d92de162dd
Rosie <jayjaygames40@gmail.com>
2026-03-11 04:21:23 -0700

    Changed structure of "rp_random.h" so that it uses the IMPLEMENTATION pattern.

--- rp_random.h
+++ rp_random.h
@@ -6,15 +6,53 @@
  * Most of it is honestly just helper functions for generating ranges and
  * shit, and I may add or remove to it as I see fit depending on whether
  * or not I even use half the stuff. Either way, it's just nice to have! :D
+ *
+ * TODO: Make the functions follow the `_*_internal()` pattern so
+ *       that debugging information can be passed through to them.
+ *
+ * MACRO DEFINE LIST:
+ *
+ * RP_RANDOM_IMPLEMENTATION:
+ *	This macro is required for all the function declarations to be
+ *	accessible. Make sure you define it before including this file
+ *	anywhere you want the code to be defined for linkage.
+ *
+ *	IMPORTANT: Make sure you only ever define this macro in
+ *	           ONE C file that gets linked with any given
+ *	           program, otherwise the linker will bitch at
+ *	           you for a multiple definition thingy-majiggy.
+ *
+ *	NOTE: All the below macros assume this one is defined.
+ *
+ * RP_RANDOM_LOG:
+ *	Prints to `stdout` whenever a random function is called to let
+ *	the user know what function was called and what number it gave.
+ *
+ * RP_RANDOM_TEST:
+ *	This defines the function definiton for testing the random module.
+ *	This is only really used in the internal `test.c` file, but you
+ *	can define it if you really wanna use it.
+ *
+ *	NOTE: All test functions in this library are static,
+ *	      meaning that they can only be defined once, although
+ *	      to be fair, you can only define the implementation once
+ *	      anyway, so it doesn't really matter all that much.
  */
 
-#include <stdlib.h>
+/* Macro parity */
+#if defined(RP_RANDOM_TEST) && !defined(RP_RANDOM_IMPLEMENTATION)
+#error "`RP_RANDOM_TEST` cannot be defined unless `RP_RANDOM_IMPLEMENTATION` also is, otherwise the function will simply not exist."
+#endif /* #if defined(RP_RANDOM_TEST) && !defined(RP_RANDOM_IMPLEMENTATION) */
+
 #include <stdint.h>
-#include <time.h>
 
 #include "rp_types.h"
 
 RP_STATIC_ASSERT(RAND_MAX == INT32_MAX, RAND_MAX_must_equal_INT32_MAX);
+
+/********************************
+ * PUBLIC FUNCTION DECLARATIONS *
+ ********************************/
 
 /*
  * You can seed the randomness with a custom value, or
@@ -27,22 +65,12 @@ RP_STATIC_ASSERT(RAND_MAX == INT32_MAX, RAND_MAX_must_equal_INT32_MAX);
  *
  * Returns the seed if the input wasn't `UINT32_MAX`.
  */
-static __inline u32 rp_random_seed(const u32 seed)
-{
-	const u32 s = (seed == UINT32_MAX) ? (u32)time(NULL) : (u32)seed;
-
-	srand(s);
-
-	return s;
-}
+extern u32 rp_random_seed(const u32 seed);
 
 /*
  * Generates a random number from 0 to 4294967295
  */
-static __inline u32 rp_random_u32(void)
-{
-	return (((u32)rand() & 0xFFFF) << 0u) | (((u32)rand() & 0xFFFF) << 16u);
-}
+extern u32 rp_random_u32(void);
 
 /*
  * Generates a random unsigned 32-bit number between a lower and upper bound
@@ -50,23 +78,12 @@ static __inline u32 rp_random_u32(void)
  * NOTE: The upper bound is exclusive, meaning if you do something
  * like `.lo = 10, .hi = 50`, you will get numbers from [10 -> 49].
  */
-static __inline u32 rp_random_u32_range(const u32 lo, const u32 hi)
-{
-	rp_assertf(hi > lo,
-		   "rp_random_u32_range(): High must be "
-		   "greater than low [.lo = %u, .hi = %u]",
-		   lo,
-		   hi);
-	return (rp_random_u32() % (hi - lo)) + lo;
-}
+extern u32 rp_random_u32_range(const u32 lo, const u32 hi);
 
 /*
  * Generates a random number from 0 to 2147483647
  */
-static __inline s32 rp_random_s32(void)
-{
-	return (s32)rp_random_u32();
-}
+extern s32 rp_random_s32(void);
 
 /*
  * Generates a random signed 32-bit number between a lower and upper bound
@@ -74,9 +91,118 @@ static __inline s32 rp_random_s32(void)
  * NOTE: The upper bound is exclusive, meaning if you do something
  * like `.lo = -10, .hi = 10`, you will get numbers from [-10 -> 9].
  */
-static __inline s32 rp_random_s32_range(const s32 lo, const s32 hi)
+extern s32 rp_random_s32_range(const s32 lo, const s32 hi);
+
+/*
+ * Generates a random number from 0 to 65535
+ */
+extern u16 rp_random_u16(void);
+
+/*
+ * Generates a random number from -32768 to 32767
+ */
+extern s16 rp_random_s16(void);
+
+/*
+ * Generates a random number from 0 to 255
+ */
+extern u8 rp_random_u8(void);
+
+/*
+ * Generates a random number from -128 to 127
+ */
+extern s8 rp_random_s8(void);
+
+/*
+ * Generates a random boolean with a 50% chance for either TRUE or FALSE
+ */
+extern bool_t rp_random_bool_50_percent(void);
+
+/*
+ * Generates a floating point number from 0 to 1
+ *
+ * It can be scaled with a magnitude, but just use `1.f` for default.
+ */
+extern f32 rp_random_f32_zo(const f32 mag);
+
+/*
+ * Generates a floating point number from -1 to 1
+ *
+ * It can be scaled with a magnitude, but just use `1.f` for default.
+ */
+extern f32 rp_random_f32_no(const f32 mag);
+
+/*******************************
+ * PUBLIC FUNCTION DEFINITIONS *
+ *******************************/
+
+#ifdef RP_RANDOM_IMPLEMENTATION
+
+#include <time.h>
+
+/*
+ * TODO:
+ * Update this so it uses the centralized logging function instead of printf.
+ */
+#ifdef debugf
+#error "`debugf()` is already defined elsewhere!"
+#endif /* #ifdef debugf */
+
+#ifdef RP_RANDOM_LOG
+#define debugf(...) printf(__VA_ARGS__)
+#else /* #ifdef RP_RANDOM_LOG */
+#define debugf(...) ((void)0)
+#endif /* #ifdef RP_RANDOM_LOG #else */
+
+u32 rp_random_seed(const u32 seed)
+{
+	const u32 s = (seed == UINT32_MAX) ? (u32)time(NULL) : (u32)seed;
+
+	srand(s);
+	debugf("RNG: rp_random_seed(%u) -> %u\n", seed, s);
+
+	return s;
+}
+
+u32 rp_random_u32(void)
+{
+	const u32 r = (((u32)rand() & 0xFFFF) << 0u) |
+		      (((u32)rand() & 0xFFFF) << 16u);
+
+	debugf("rp_random_u32() -> %u\n", r);
+
+	return r;
+}
+
+u32 rp_random_u32_range(const u32 lo, const u32 hi)
+{
+	u32 r;
+
+	rp_assertf(hi > lo,
+		   "rp_random_u32_range(): High must be "
+		   "greater than low [.lo = %u, .hi = %u]",
+		   lo,
+		   hi);
+
+	r = (rp_random_u32() % (hi - lo)) + lo;
+	debugf("rp_random_u32_range(lo:%u, hi:%u) -> %u\n", lo, hi, r);
+
+	return r;
+}
+
+s32 rp_random_s32(void)
+{
+	const s32 r = (s32)rp_random_u32();
+
+	debugf("rp_random_s32() -> %d\n", r);
+
+	return r;
+}
+
+s32 rp_random_s32_range(const s32 lo, const s32 hi)
 {
 	s64 t;
+	s32 r;
 
 	rp_assertf(hi > lo,
 		   "rp_random_s32_range(): High must be "
@@ -91,73 +217,78 @@ static __inline s32 rp_random_s32_range(const s32 lo, const s32 hi)
 		   t,
 		   (s32)t);
 
-	return (s32)t;
+	r = (s32)t;
+	debugf("rp_random_s32_range(lo:%d, hi:%d) -> %d\n", lo, hi, r);
+
+	return r;
 }
 
-/*
- * Generates a random number from 0 to 65535
- */
-static __inline u16 rp_random_u16(void)
+u16 rp_random_u16(void)
 {
-	return (u16)(rp_random_u32() & 0xFFFF);
+	const u16 r = (u16)(rp_random_u32() & 0xFFFF);
+
+	debugf("rp_random_u16() -> %u\n", r);
+
+	return r;
 }
 
-/*
- * Generates a random number from -32768 to 32767
- */
-static __inline s16 rp_random_s16(void)
+s16 rp_random_s16(void)
 {
-	return (s16)(rp_random_u32() & 0xFFFF);
+	const s16 r = (s16)(rp_random_u32() & 0xFFFF);
+
+	debugf("rp_random_s16() -> %d\n", r);
+
+	return r;
 }
 
-/*
- * Generates a random number from 0 to 255
- */
-static __inline u8 rp_random_u8(void)
+u8 rp_random_u8(void)
 {
-	return (u8)(rp_random_u32() & 0xFF);
+	const u8 r = (u8)(rp_random_u32() & 0xFF);
+
+	debugf("rp_random_u8() -> %u\n", r);
+
+	return r;
 }
 
-/*
- * Generates a random number from -128 to 127
- */
-static __inline s8 rp_random_s8(void)
+s8 rp_random_s8(void)
 {
-	return (s8)(rp_random_u32() & 0xFF);
+	const s8 r = (s8)(rp_random_u32() & 0xFF);
+
+	debugf("rp_random_u8() -> %d\n", r);
+
+	return r;
 }
 
-/*
- * Generates a random boolean with a 50% chance for either TRUE or FALSE
- */
-static __inline bool_t rp_random_bool_50_percent(void)
+bool_t rp_random_bool_50_percent(void)
 {
-	return (bool_t)(rand() & 1);
+	const bool_t r = (bool_t)(rand() & 1);
+
+	debugf("rp_random_bool_50_percent() -> %5s\n", r ? "TRUE" : "FALSE");
+
+	return r;
 }
 
-/*
- * Generates a floating point number from 0 to 1
- *
- * It can be scaled with a magnitude, but just use `1.f` for default.
- */
-static __inline f32 rp_random_f32_zo(const f32 mag)
+f32 rp_random_f32_zo(const f32 mag)
 {
-	return mag * ((f32)rand() / (f32)RAND_MAX);
+	const f32 r = mag * ((f32)rand() / (f32)RAND_MAX);
+
+	debugf("rp_random_f32_zo(mag:%f) -> %f\n", (f64)mag, (f64)r);
+
+	return r;
 }
 
-/*
- * Generates a floating point number from -1 to 1
- *
- * It can be scaled with a magnitude, but just use `1.f` for default.
- */
-static __inline f32 rp_random_f32_no(const f32 mag)
+f32 rp_random_f32_no(const f32 mag)
 {
-	return mag * (((f32)rand() / (f32)(RAND_MAX >> 1)) - 1.f);
+	const f32 r = mag * (((f32)rand() / (f32)(RAND_MAX >> 1)) - 1.f);
+
+	debugf("rp_random_f32_no(mag:%f) -> %f\n", (f64)mag, (f64)r);
+
+	return r;
 }
 
 #ifdef RP_RANDOM_TEST
-#include <stdio.h>
 
-static __inline void rp_random_test(void)
+static void rp_random_test(void)
 {
 	s32 s32_min, s32_max;
 	u32 u32_min, u32_max;
@@ -195,5 +326,7 @@ static __inline void rp_random_test(void)
 	printf("F32 [-4096 -> 4096]: %f\n", (f64)rp_random_f32_no(4096.f));
 }
 #endif /* #ifdef RP_RANDOM_TEST */
+
+#endif /* #ifdef RP_RANDOM_IMPLEMENTATION */
 
 #endif /* #ifndef _RP_RANDOM_H_ */
--- test.c
+++ test.c
@@ -1,5 +1,13 @@
+/*
+ * TODO: Add a separate `rp_log.h` file that can be toggled with
+ *       `RP_LOG_ENABLE`. Module-specific macros like `RP_MEMORY_LOG`
+ *       and `RP_RANDOM_LOG` will turn on and off this macro for
+ *       those specific modules depending on what is necessary.
+ */
+
 /* rp_random.h */
 #define RP_RANDOM_TEST
+#define RP_RANDOM_IMPLEMENTATION
 #include "rp_random.h"
 
 /* rp_memory.h */
@@ -13,6 +21,5 @@ int main(void)
 {
 	rp_random_test();
 	rp_memory_test();
-
 	return 0;
 }
